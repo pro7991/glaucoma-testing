951e9ccaaf71e2d9d817e3358f179552a9b52290
luong.nguyen <luong.nguyen@evizi.com>
2023-03-08 16:57:04 +0700

    Update package

---
+++ src/com/glucoma/uat/InternalTRF.java
@@ -0,0 +1,24 @@
+package com.glucoma.uat;
+
+import java.io.IOException;
+import java.net.URL;
+
+import com.glucoma.creater.Environment;
+import com.glucoma.creater.TRFInternalCreater;
+
+public class InternalTRF {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		TRFInternalCreater creater = new TRFInternalCreater(getParentPath() + "trf-internal-user.xlsx", Environment.UAT);
+		creater.start();
+//		TRFInternalUpdater updater = new TRFInternalUpdater("trf-internal-user.xlsx");
+//		updater.start();
+	}
+
+	private static String getParentPath() {
+		URL location = new InternalTRF().getClass().getResource("InternalTRF.class");
+        String ref = location.getPath();
+        return ref.substring(0, ref.lastIndexOf("/")+1);
+	}
+
+}
